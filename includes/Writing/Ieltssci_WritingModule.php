1acc7870f98745f88ab8140bba592abe0b05dc62
bi1101 <bihungba1101@GMAIL.COM>
2025-01-20 17:38:08 +0700

    Enhance writing module asset enqueueing by checking assigned pages and localizing script data

--- includes/Writing/Ieltssci_WritingModule.php
+++ includes/Writing/Ieltssci_WritingModule.php
@@ -12,6 +12,14 @@ class Ieltssci_WritingModule {
 		add_filter( 'ielts_science_lms_module_pages_data', [ $this, 'provide_module_pages_data' ] );
 	}
 
+	/**
+	 * Đăng ký các tài nguyên (assets) cho module viết.
+	 *
+	 * Hàm này sẽ tìm và đăng ký các tệp JavaScript và CSS cần thiết cho module viết.
+	 * Các tệp tài nguyên được tìm kiếm trong thư mục 'public/writing/build/'.
+	 * 
+	 * @return void
+	 */
 	public function register_writing_assets() {
 		$build_path = plugin_dir_path( __FILE__ ) . '../../public/writing/build/';
 		$asset_files = glob( $build_path . '*.asset.php' );
@@ -60,7 +68,30 @@ class Ieltssci_WritingModule {
 	}
 
 	public function enqueue_writing_assets() {
-		if ( is_singular( 'page' ) && get_post_meta( get_the_ID(), 'ieltssci_flag', true ) === 'IELTS Science Writing' ) {
+		// Get the saved page settings
+		$ielts_pages = get_option( 'ielts_science_lms_pages', [] );
+
+		// Get module pages data using the filter
+		$module_pages_data = apply_filters( 'ielts_science_lms_module_pages_data', [] );
+
+		// Extract writing module pages
+		$writing_module_pages = [];
+		if ( isset( $module_pages_data['writing_module']['pages'] ) ) {
+			$writing_module_pages = $module_pages_data['writing_module']['pages'];
+		}
+
+		// Check if current page is one of the assigned writing module pages
+		$should_enqueue = false;
+		if ( ! empty( $ielts_pages ) && ! empty( $writing_module_pages ) ) {
+			foreach ( $writing_module_pages as $page_key => $page_label ) {
+				if ( isset( $ielts_pages[ $page_key ] ) && is_page( $ielts_pages[ $page_key ] ) ) {
+					$should_enqueue = true;
+					break;
+				}
+			}
+		}
+
+		if ( $should_enqueue ) {
 			// Define the handle for the index script and style.
 			$script_handle = 'ielts-science-index';
 			$style_handle = 'ielts-science-index-css';
@@ -75,11 +106,21 @@ class Ieltssci_WritingModule {
 				wp_enqueue_script( $script_handle );
 			}
 
-
 			// Enqueue the index style if it's registered.
 			if ( wp_style_is( $style_handle, 'registered' ) ) {
 				wp_enqueue_style( $style_handle );
 			}
+
+			// Prepare data for localization using writing module pages
+			$page_data_for_js = [];
+			foreach ( $writing_module_pages as $page_key => $page_label ) {
+				if ( isset( $ielts_pages[ $page_key ] ) ) {
+					$page_data_for_js[ $page_key ] = get_page_uri( $ielts_pages[ $page_key ] );
+				}
+			}
+
+			// Localize script (pass data to the React app)
+			wp_localize_script( $script_handle, 'ielts_writing_data', $page_data_for_js );
 		}
 	}
 
