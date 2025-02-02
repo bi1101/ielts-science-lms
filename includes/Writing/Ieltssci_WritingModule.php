dfb66fc0aaf68bad237843347c54aca4229b8673
bi1101 <bihungba1101@GMAIL.COM>
2025-02-02 17:01:19 +0700

    Refactor menu retrieval functions to improve icon handling and add account menu support

--- includes/Writing/Ieltssci_WritingModule.php
+++ includes/Writing/Ieltssci_WritingModule.php
@@ -132,100 +132,111 @@ class Ieltssci_WritingModule {
 
 			// --- Menu Retrieval and Localization ---
 
-			$menu_name = 'header-menu'; // Or your specific theme location
-			$menu_items = array();
+			// Function to get the menu item icon
+			function get_menu_item_icon( $item ) {
+				// Use locate_template to find the class files, respecting child themes
+				$meta_file = locate_template( 'inc/plugins/buddyboss-menu-icons/includes/meta.php' );
+				$front_file = locate_template( 'inc/plugins/buddyboss-menu-icons/includes/front.php' );
 
-			if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
-				$menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
-				$menu_items = wp_get_nav_menu_items( $menu->term_id, array( 'order' => 'ASC' ) );
-
-				// Function to get the menu item icon
-				function get_menu_item_icon( $item ) {
-					// Use locate_template to find the class files, respecting child themes
-					$meta_file = locate_template( 'inc/plugins/buddyboss-menu-icons/includes/meta.php' );
-					$front_file = locate_template( 'inc/plugins/buddyboss-menu-icons/includes/front.php' );
-
-					// Include the necessary class files if they exist
-					if ( file_exists( $meta_file ) ) {
-						require_once( $meta_file );
-					}
-					if ( file_exists( $front_file ) ) {
-						require_once( $front_file );
-					}
-
-					$icon = false;
-
-					// Use fully qualified class names with leading backslash to indicate global namespace
-					if ( class_exists( '\Menu_Icons_Meta' ) ) {
-						$meta = \Menu_Icons_Meta::get( $item->ID );
-						if ( class_exists( '\Menu_Icons_Front_End' ) ) {
-							$icon = \Menu_Icons_Front_End::get_icon( $meta );
-						}
-					}
-
-					// Handle cases where the icon might be an HTML string or an object
-					if ( is_object( $icon ) && isset( $icon->html ) ) {
-						$icon = $icon->html;
-					} elseif ( is_array( $icon ) && isset( $icon['html'] ) ) {
-						$icon = $icon['html'];
-					}
-
-					// Sanitize the icon HTML
-					if ( $icon ) {
-						$icon = wp_kses(
-							$icon,
-							array(
-								'i' => array(
-									'class' => array(),
-								),
-								'svg' => array(
-									'class' => array(),
-									'aria-hidden' => array(),
-									'role' => array(),
-									'focusable' => array(),
-									'xmlns' => array(),
-									'width' => array(),
-									'height' => array(),
-									'viewbox' => array(),
-								),
-								'path' => array(
-									'd' => array(),
-									'fill' => array(),
-								),
-								'use' => array(
-									'xlink:href' => array(),
-								),
-							)
-						);
-					}
-
-					return $icon;
+				// Include the necessary class files if they exist
+				if ( file_exists( $meta_file ) ) {
+					require_once( $meta_file );
+				}
+				if ( file_exists( $front_file ) ) {
+					require_once( $front_file );
 				}
 
-				// Function to build the hierarchical menu structure
-				function build_hierarchical_menu( $items, $parent = 0 ) {
-					$menu = array();
-					foreach ( $items as $item ) {
-						if ( $item->menu_item_parent == $parent ) {
-							$menu_item = array(
-								'id' => $item->ID,
-								'title' => $item->title,
-								'url' => $item->url,
-								'children' => build_hierarchical_menu( $items, $item->ID ), // Recursively find children
-								'icon' => get_menu_item_icon( $item ), // Get the icon data
-								// ... add other fields you need from the menu item object (e.g., classes, target, etc.) ...
-							);
-							$menu[] = $menu_item;
-						}
+				$icon = false;
+
+				// Use fully qualified class names with leading backslash to indicate global namespace
+				if ( class_exists( '\Menu_Icons_Meta' ) ) {
+					$meta = \Menu_Icons_Meta::get( $item->ID );
+					if ( class_exists( '\Menu_Icons_Front_End' ) ) {
+						$icon = \Menu_Icons_Front_End::get_icon( $meta );
 					}
-					return $menu;
 				}
 
-				$formatted_menu_items = build_hierarchical_menu( $menu_items );
-			} else {
-				$formatted_menu_items = array(); // Empty array if menu not found
+				// Handle cases where the icon might be an HTML string or an object
+				if ( is_object( $icon ) && isset( $icon->html ) ) {
+					$icon = $icon->html;
+				} elseif ( is_array( $icon ) && isset( $icon['html'] ) ) {
+					$icon = $icon['html'];
+				}
+
+				// Sanitize the icon HTML
+				if ( $icon ) {
+					$icon = wp_kses(
+						$icon,
+						array(
+							'i' => array(
+								'class' => array(),
+							),
+							'svg' => array(
+								'class' => array(),
+								'aria-hidden' => array(),
+								'role' => array(),
+								'focusable' => array(),
+								'xmlns' => array(),
+								'width' => array(),
+								'height' => array(),
+								'viewbox' => array(),
+							),
+							'path' => array(
+								'd' => array(),
+								'fill' => array(),
+							),
+							'use' => array(
+								'xlink:href' => array(),
+							),
+						)
+					);
+				}
+
+				return $icon;
 			}
 
+			// Function to build the hierarchical menu structure
+			function build_hierarchical_menu( $items, $parent = 0 ) {
+				$menu = array();
+				foreach ( $items as $item ) {
+					if ( $item->menu_item_parent == $parent ) {
+						$menu_item = array(
+							'id' => $item->ID,
+							'title' => $item->title,
+							'url' => html_entity_decode( $item->url ),
+							'children' => build_hierarchical_menu( $items, $item->ID ), // Recursively find children
+							'icon' => get_menu_item_icon( $item ), // Get the icon data
+							// ... add other fields you need from the menu item object (e.g., classes, target, etc.) ...
+						);
+						$menu[] = $menu_item;
+					}
+				}
+				return $menu;
+			}
+
+			// --- Header Menu ---
+			$header_menu_name = 'header-menu';
+			$header_menu_items = array();
+
+			if ( ( $header_locations = get_nav_menu_locations() ) && isset( $header_locations[ $header_menu_name ] ) ) {
+				$header_menu = wp_get_nav_menu_object( $header_locations[ $header_menu_name ] );
+				$header_menu_items = wp_get_nav_menu_items( $header_menu->term_id, array( 'order' => 'ASC' ) );
+				$formatted_header_menu_items = build_hierarchical_menu( $header_menu_items );
+			} else {
+				$formatted_header_menu_items = array(); // Empty array if menu not found
+			}
+
+			// --- Account Menu ---
+			$account_menu_name = 'header-my-account';
+			$account_menu_items = array();
+
+			if ( ( $account_locations = get_nav_menu_locations() ) && isset( $account_locations[ $account_menu_name ] ) ) {
+				$account_menu = wp_get_nav_menu_object( $account_locations[ $account_menu_name ] );
+				$account_menu_items = wp_get_nav_menu_items( $account_menu->term_id, array( 'order' => 'ASC' ) );
+				$formatted_account_menu_items = build_hierarchical_menu( $account_menu_items );
+			} else {
+				$formatted_account_menu_items = array();
+			}
 			// --- End of Menu Retrieval ---
 
 			// Combine all data to be localized
@@ -234,7 +245,8 @@ class Ieltssci_WritingModule {
 				'nonce' => $nonce,
 				'root_url' => $root_url,
 				'is_logged_in' => is_user_logged_in(),
-				'header_menu' => $formatted_menu_items,
+				'header_menu' => $formatted_header_menu_items,
+				'account_menu' => $formatted_account_menu_items,
 			];
 
 			// Localize script (pass data to the React app)
