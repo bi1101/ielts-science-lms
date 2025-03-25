0ae9809f5314153982191354dda03c7e1fece4ce
bi1101 <bihungba1101@GMAIL.COM>
2025-03-25 16:22:34 +0700

    feat: add language file for translations and update script registration

--- includes/Writing/Ieltssci_Writing_Module.php
+++ includes/Writing/Ieltssci_Writing_Module.php
@@ -54,6 +54,7 @@ class Ieltssci_Writing_Module {
 			$ver    = $asset['version'];
 
 			wp_register_script( $handle, $src, $deps, $ver, true );
+			wp_set_script_translations( $handle, 'ielts-science-lms', plugin_dir_path( __FILE__ ) . '../../languages' );
 
 			$css_file = str_replace( '.asset.php', '.css', $asset_file );
 			if ( file_exists( $css_file ) ) {
