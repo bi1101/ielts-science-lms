98f14a36d9c93e436d6c7a00ed408082083efe15
bi1101 <bihungba1101@GMAIL.COM>
2025-09-11 17:33:55 +0700

    feat: Add embeddable link for task submissions in essay response

--- includes/Writing/Ieltssci_Writing_Essay_Controller.php
+++ includes/Writing/Ieltssci_Writing_Essay_Controller.php
@@ -1925,6 +1925,15 @@ class Ieltssci_Writing_Essay_Controller extends WP_REST_Controller {
 			);
 		}
 
+		// Add embeddable link to the task submissions for this essay.
+		$response->add_link(
+			'task_submissions',
+			rest_url( 'ieltssci/v1/writing-task-submissions?essay_id=' . $essay['id'] ),
+			array(
+				'embeddable' => true,
+			)
+		);
+
 		/**
 		 * Filter essay data returned from the REST API.
 		 *
