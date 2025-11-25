c543bbbdbaa02b9429c59203da47e1cb9e066b0d
bi1101 <bihungba1101@GMAIL.COM>
2025-11-25 15:09:09 +0700

    chore: bump version to 0.11.17 in plugin header and package.json

--- ielts-science-lms.php
+++ ielts-science-lms.php
@@ -3,7 +3,7 @@
  * Plugin Name: IELTS Science LMS
  * Plugin URI: https://ieltsscience.fun/
  * Description: IELTS Science Learning Management System.
- * Version: 0.11.16
+ * Version: 0.11.17
  * Author: IELTS Science
  * Author URI: https://ieltsscience.fun/
  * Text Domain: ielts-science-lms
